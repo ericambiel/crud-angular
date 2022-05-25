0a7775b93b961c68b5779ab793efca017ecd0c79
Eric Ambiel <eric.ambiel@gmail.com>
2022-05-25 16:53:24 -0300

    feat(class-04): add new component courses.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'courses',
+  },
+  {
+    path: 'courses',
+    loadChildren: () =>
+      import('./courses/courses.module').then(m => m.CoursesModule), // TODO: study about lazy load route
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
